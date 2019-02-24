702e4cb19c82d7811f1d72b262f8e415bc549933
chenminhua <chenmh@blacklake.cn>
2019-02-24 23:51:21 +0800

    admin dashboard

---
+++ dashboard/eurekaServer/src/main/java/com/chenminhua/eurekaserver/EurekaServiceApp.java
@@ -0,0 +1,13 @@
+package com.chenminhua.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApp.class);
+    }
+}
